95167c71b38d46f4d4bca143b19a3de0b2b9b916
Keith Dahlby <dahlbyk@gmail.com>
2021-03-26 14:59:49 -0500

    Start on LinkedList

---
+++ DataStructures/LinkedLists/LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataStructures.LinkedLists
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public void Insert(int value)
+        {
+            Head = new Node(value);
+        }
+
+        public bool Include(int valueToFind)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
