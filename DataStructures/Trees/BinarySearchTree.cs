3f5306f6c01ce7e94ad5bbb28c8b34ee88dfa0e2
Keith Dahlby <keith@newbo.co>
2021-04-09 14:37:00 -0500

    Stub in BinaryTree and BinarySearchTree

---
+++ DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructures.Trees
+{
+    public class BinarySearchTree : BinaryTree
+    {
+        public void Add(int number)
+        {
+        }
+
+        public bool Contains(int number)
+        {
+            return false;
+        }
+    }
+}
