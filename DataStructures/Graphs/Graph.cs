4441617a47cd73f0d96261a5a9f57f10a0dd8a8b
Keith Dahlby <keith@newbo.co>
2021-05-10 09:43:08 -0500

    Stub in Graph

---
+++ DataStructures/Graphs/Graph.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graphs
+{
+    public class Graph<TValue, TEdge>
+    {
+        public void AddNode(TValue value)
+        {
+        }
+
+        /// <summary>
+        /// Add directed edge from <paramref name="from"/> to <paramref name="to"/>.
+        /// </summary>
+        public void AddEdge(TValue from, TValue to, TEdge data = default)
+        {
+        }
+
+        public IEnumerable<Node> Nodes => throw new NotImplementedException();
+
+        public IEnumerable<Edge> GetNeighbors(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public class Node
+        {
+        }
+
+        public class Edge
+        {
+        }
+    }
+}
